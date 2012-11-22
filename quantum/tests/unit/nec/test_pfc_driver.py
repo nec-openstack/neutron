6d815f7d2a5595795b613b796143b7f731caa0b4
Akihiro MOTOKI <motoki@da.jp.nec.com>
2012-11-22 09:32:10 +0900

    Update unittest for PFC driver.
    After integration test with PFC, PFC driver was modified.
    Thus the unittest needs to be updated.

--- quantum/tests/unit/nec/test_pfc_driver.py
+++ quantum/tests/unit/nec/test_pfc_driver.py
@@ -38,17 +38,20 @@ def _ofc(id):
     return "ofc-%s" % id
 
 
-class PFCDriverTestBase(unittest.TestCase):
+class PFCDriverTestBase():
+
+    driver = 'quantum.plugins.nec.drivers.pfc.PFCDriverBase'
 
     def setUp(self):
         self.mox = mox.Mox()
-        self.driver = drivers.get_driver("pfc")(TestConfig)
+        self.driver = drivers.get_driver(self.driver)(TestConfig)
         self.mox.StubOutWithMock(ofc.OFCClient, 'do_request')
 
     def tearDown(self):
         self.mox.UnsetStubs()
 
-    def get_ofc_item_random_params(self):
+    @staticmethod
+    def get_ofc_item_random_params():
         """create random parameters for ofc_item test"""
         tenant_id = utils.str_uuid()
         network_id = utils.str_uuid()
@@ -58,31 +61,41 @@ class PFCDriverTestBase(unittest.TestCase):
                                     mac="11:22:33:44:55:66")
         return tenant_id, network_id, portinfo
 
-    def testa_create_tenant(self):
-        t, n, p = self.get_ofc_item_random_params()
+    @staticmethod
+    def _generate_ofc_tenant_id(tenant_id):
+        fields = tenant_id.split('-')
+        # Strip 1st character (UUID version) of 3rd field
+        fields[2] = fields[2][1:]
+        return ''.join(fields)
+
+    @staticmethod
+    def get_ofc_description(desc):
+        """OFC description consists of [A-Za-z0-9_]."""
+        return desc.replace('-', '_').replace(' ', '_')
+
+    def _create_tenant(self, t, ofc_t, post_id=False):
+        tenant_path = '/tenants/%s' % ofc_t
         description = "desc of %s" % t
+        ofc_description = self.get_ofc_description(description)
 
         path = "/tenants"
-        body = {'id': t, 'description': description}
-        tenant = {'id': _ofc(t)}
-        ofc.OFCClient.do_request("POST", path, body=body).AndReturn(tenant)
+        body = {'description': ofc_description}
+        if post_id:
+            body['id'] = ofc_t
+            ofc.OFCClient.do_request("POST", path, body=body)
+        else:
+            ofc.OFCClient.do_request("POST", path, body=body).\
+                AndReturn({'id': ofc_t})
         self.mox.ReplayAll()
 
         ret = self.driver.create_tenant(description, t)
         self.mox.VerifyAll()
-        self.assertEqual(ret, _ofc(t))
+        self.assertEqual(ret, tenant_path)
 
-    def testb_update_tenant(self):
+    def testa_create_tenant(self):
         t, n, p = self.get_ofc_item_random_params()
-        description = "new desc of %s" % t
-
-        path = "/tenants/%s" % _ofc(t)
-        body = {'description': description}
-        ofc.OFCClient.do_request("PUT", path, body=body)
-        self.mox.ReplayAll()
-
-        self.driver.update_tenant(_ofc(t), description)
-        self.mox.VerifyAll()
+        ofc_t = self._generate_ofc_tenant_id(t)
+        self._create_tenant(t, ofc_t)
 
     def testc_delete_tenant(self):
         t, n, p = self.get_ofc_item_random_params()
@@ -91,68 +104,101 @@ class PFCDriverTestBase(unittest.TestCase):
         ofc.OFCClient.do_request("DELETE", path)
         self.mox.ReplayAll()
 
-        self.driver.delete_tenant(_ofc(t))
+        self.driver.delete_tenant(path)
         self.mox.VerifyAll()
 
     def testd_create_network(self):
         t, n, p = self.get_ofc_item_random_params()
         description = "desc of %s" % n
+        ofc_description = self.get_ofc_description(description)
 
-        path = "/tenants/%s/networks" % _ofc(t)
-        body = {'id': n, 'description': description}
+        tenant_path = "/tenants/%s" % _ofc(t)
+        post_path = "%s/networks" % tenant_path
+        body = {'description': ofc_description}
         network = {'id': _ofc(n)}
-        ofc.OFCClient.do_request("POST", path, body=body).AndReturn(network)
+        ofc.OFCClient.do_request("POST", post_path, body=body).\
+            AndReturn(network)
         self.mox.ReplayAll()
 
-        ret = self.driver.create_network(_ofc(t), description, n)
+        ret = self.driver.create_network(tenant_path, description, n)
         self.mox.VerifyAll()
-        self.assertEqual(ret, _ofc(n))
+        net_path = "/tenants/%s/networks/%s" % (_ofc(t), _ofc(n))
+        self.assertEqual(ret, net_path)
 
     def teste_update_network(self):
         t, n, p = self.get_ofc_item_random_params()
         description = "desc of %s" % n
+        ofc_description = self.get_ofc_description(description)
 
-        path = "/tenants/%s/networks/%s" % (_ofc(t), _ofc(n))
-        body = {'description': description}
-        ofc.OFCClient.do_request("PUT", path, body=body)
+        net_path = "/tenants/%s/networks/%s" % (_ofc(t), _ofc(n))
+        body = {'description': ofc_description}
+        ofc.OFCClient.do_request("PUT", net_path, body=body)
         self.mox.ReplayAll()
 
-        self.driver.update_network(_ofc(t), _ofc(n), description)
+        self.driver.update_network(_ofc(t), net_path, description)
         self.mox.VerifyAll()
 
     def testf_delete_network(self):
         t, n, p = self.get_ofc_item_random_params()
 
-        path = "/tenants/%s/networks/%s" % (_ofc(t), _ofc(n))
-        ofc.OFCClient.do_request("DELETE", path)
+        net_path = "/tenants/%s/networks/%s" % (_ofc(t), _ofc(n))
+        ofc.OFCClient.do_request("DELETE", net_path)
         self.mox.ReplayAll()
 
-        self.driver.delete_network(_ofc(t), _ofc(n))
+        self.driver.delete_network(_ofc(t), net_path)
         self.mox.VerifyAll()
 
     def testg_create_port(self):
         t, n, p = self.get_ofc_item_random_params()
 
-        path = "/tenants/%s/networks/%s/ports" % (_ofc(t), _ofc(n))
-        body = {'id': p.id,
-                'datapath_id': p.datapath_id,
+        net_path = "/tenants/%s/networks/%s" % (_ofc(t), _ofc(n))
+        post_path = "%s/ports" % net_path
+        port_path = "/tenants/%s/networks/%s/ports/%s" % (_ofc(t), _ofc(n),
+                                                          _ofc(p.id))
+        body = {'datapath_id': p.datapath_id,
                 'port': str(p.port_no),
                 'vid': str(p.vlan_id)}
         port = {'id': _ofc(p.id)}
-        ofc.OFCClient.do_request("POST", path, body=body).AndReturn(port)
+        ofc.OFCClient.do_request("POST", post_path, body=body).AndReturn(port)
         self.mox.ReplayAll()
 
-        ret = self.driver.create_port(_ofc(t), _ofc(n), p, p.id)
+        ret = self.driver.create_port(_ofc(t), net_path, p, p.id)
         self.mox.VerifyAll()
-        self.assertEqual(ret, _ofc(p.id))
+        self.assertEqual(ret, port_path)
 
     def testh_delete_port(self):
         t, n, p = self.get_ofc_item_random_params()
 
-        path = "/tenants/%s/networks/%s/ports/%s" % (_ofc(t), _ofc(n),
-                                                     _ofc(p.id))
-        ofc.OFCClient.do_request("DELETE", path)
+        net_path = "/tenants/%s/networks/%s" % (_ofc(t), _ofc(n))
+        port_path = "/tenants/%s/networks/%s/ports/%s" % (_ofc(t), _ofc(n),
+                                                          _ofc(p.id))
+        ofc.OFCClient.do_request("DELETE", port_path)
         self.mox.ReplayAll()
 
-        self.driver.delete_port(_ofc(t), _ofc(n), _ofc(p.id))
+        self.driver.delete_port(_ofc(t), net_path, port_path)
         self.mox.VerifyAll()
+
+
+class PFCDriverBaseTest(PFCDriverTestBase, unittest.TestCase):
+    pass
+
+
+class PFCV3DriverTest(PFCDriverTestBase, unittest.TestCase):
+
+    driver = 'pfc_v3'
+
+    def testa_create_tenant(self):
+        pass
+
+    def testc_delete_tenant(self):
+        pass
+
+
+class PFCV4DriverTest(PFCDriverTestBase, unittest.TestCase):
+
+    driver = 'pfc_v4'
+
+    def testa_create_tenant(self):
+        t, n, p = self.get_ofc_item_random_params()
+        ofc_t = self._generate_ofc_tenant_id(t)
+        self._create_tenant(t, ofc_t, post_id=True)
